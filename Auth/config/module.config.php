76d2c0f3e94eed384ff447b5f2317f9eb5aae446
Josué <dev@josuerockefeller.com>
2017-06-07 23:58:45 -0300

    Html remove

--- Auth/config/module.config.php
+++ Auth/config/module.config.php
@@ -38,22 +38,7 @@ return array(
         'invokables' => array(
         ),
     ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
-        'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'login/index/index' => __DIR__ . '/../view/login/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
+   
     // Placeholder for console routes
     'console' => array(
         'router' => array(
